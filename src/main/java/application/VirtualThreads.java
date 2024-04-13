0cba9d71cdfed0f2b7979de9aa248e0e818c70c0
renzozuk <renzo.zuk19@gmail.com>
2024-04-13 16:43:16 -0300

    The application package is more organized now.

---
+++ src/main/java/application/VirtualThreads.java
@@ -0,0 +1,23 @@
+package application;
+
+import services.ExecutionService;
+
+import java.io.FileNotFoundException;
+
+public class VirtualThreads {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+        long checkpoint1 = System.currentTimeMillis();
+
+        ExecutionService.runVirtualThreads(ExecutionService.getDefaultDatabases());
+
+        long checkpoint2 = System.currentTimeMillis();
+
+        ExecutionService.runVirtualThreads(ExecutionService.getDefaultTasks());
+
+        long checkpoint3 = System.currentTimeMillis();
+
+        System.out.printf("Time to read the database: %.3fs%n", (checkpoint2 - checkpoint1) / 1e3);
+        System.out.printf("Time to export the required locations: %.3fs%n", (checkpoint3 - checkpoint2) / 1e3);
+        System.out.printf("Total time: %.3fs%n", (checkpoint3 - checkpoint1) / 1e3);
+    }
+}
---
+++ src/main/java/services/ExecutionService.java
@@ -0,0 +1,83 @@
+package services;
+
+import entities.Point;
+import util.MomentIterator;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class ExecutionService {
+    public static void runSequential(List<Runnable> runnables) {
+        runnables.forEach(Runnable::run);
+    }
+
+    public static void runPlatformThreads(List<Runnable> runnables) throws InterruptedException {
+        runPlatformThreads(runnables, Thread.MIN_PRIORITY);
+    }
+
+    public static void runVirtualThreads(List<Runnable> runnables) throws InterruptedException {
+        runVirtualThreads(runnables, Thread.MIN_PRIORITY);
+    }
+
+    public static void runPlatformThreads(List<Runnable> runnables, int priority) throws InterruptedException {
+        List<Thread> threads = runnables.stream().map(r -> Thread.ofPlatform().name(r.getClass().getSimpleName().split("/")[1]).start(r)).toList();
+
+        for(Thread thread : threads){
+            thread.setPriority(priority);
+            thread.join();
+        }
+    }
+
+    public static void runVirtualThreads(List<Runnable> runnables, int priority) throws InterruptedException {
+        List<Thread> threads = runnables.stream().map(r -> Thread.ofVirtual().name(r.getClass().getSimpleName().split("/")[1]).start(r)).toList();
+
+        for(Thread thread : threads){
+            thread.setPriority(priority);
+            thread.join();
+        }
+    }
+
+    public static List<Runnable> getDefaultDatabases() {
+        Runnable firstDatabase = () -> {
+            try {
+                FileManagementService.importDatabase("Brazil - 2021");
+                FileManagementService.importDatabase("Uruguay - 2021");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Runnable secondDatabase = () -> {
+            try {
+                FileManagementService.importDatabase("Brazil - 2022");
+                FileManagementService.importDatabase("Uruguay - 2022");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Runnable thirdDatabase = () -> {
+            try {
+                FileManagementService.importDatabase("Brazil - 2023");
+                FileManagementService.importDatabase("Uruguay - 2023");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        return List.of(firstDatabase, secondDatabase, thirdDatabase);
+    }
+
+    public static List<Runnable> getDefaultTasks() throws FileNotFoundException {
+        List<Point> locations = FileManagementService.importLocations();
+
+        Runnable firstTask = () -> InterpolationService.exportInterpolation(new MomentIterator("01/01/2021", "31/12/2023"), locations.subList(0, locations.size() / 3));
+
+        Runnable secondTask = () -> InterpolationService.exportInterpolation(new MomentIterator("01/01/2021", "31/12/2023"), locations.subList(locations.size() / 3, locations.size() / 3 * 2));
+
+        Runnable thirdTask = () -> InterpolationService.exportInterpolation(new MomentIterator("01/01/2021", "31/12/2023"), locations.subList(locations.size() / 3 * 2, locations.size()));
+
+        return List.of(firstTask, secondTask, thirdTask);
+    }
+}
